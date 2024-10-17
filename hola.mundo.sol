006a73f18feabd664419cfba553c38472769a0d5
Kalchaqui <diego.rbarrionuevo@gmail.com>
2024-10-17 17:55:30 -0300

    Cambios

---
+++ hola.mundo.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; //defino versión
+
+contract hola_mundo { //a partir de ahora la lógica del contrato
+    string public saludo = "Hola mundo desde educateth";
+    
+
+}
